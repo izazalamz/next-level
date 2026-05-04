aee52b57c9ea2fec731ebf73a98d70c39bf9ef46
izazalamz <izaz.32.alam@gmail.com>
2026-05-04 12:01:24 +0600

    binary search

---
+++ mission-1/module-4/4-6-binary search.js
@@ -0,0 +1,16 @@
+const binarySearch = (arr, target) => {
+  let low = 0;
+  let high = arr.length - 1;
+  while (low <= high) {
+    let mid = Math.floor(low + (high - low) / 2);
+    if (arr[mid] === target) {
+      return [arr[mid], mid];
+    } else if (target < arr[mid]) {
+      high = mid - 1;
+    } else if (target > arr[mid]) {
+      low = mid + 1;
+    }
+  }
+  return -1;
+};
+console.log(binarySearch([1, 2, 3, 4, 5, 6, 7, 8, 9], 1));
